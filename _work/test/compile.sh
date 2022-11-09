c564f1c4f8d151814d2b3ece9644a9009eab3132
Vincent Michaud-Rioux <vincentm@nanoacademic.com>
2022-11-09 17:38:08 -0500

    Add simple compilation test.

---
+++ _work/test/compile.sh
@@ -0,0 +1,8 @@
+ROOT=$PWD/../../
+g++ test.cpp \
+-I${ROOT}/build/temp.linux-x86_64-3.8/_deps/kokkos-src/core/src \
+-I${ROOT}/build/_deps/kokkos-build \
+${ROOT}/build/_deps/kokkos-build/core/src/libkokkoscore.a \
+-ldl \
+-fopenmp \
+-std=c++2a
