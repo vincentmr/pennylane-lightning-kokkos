70ed1ab4cf96eae3771303f9c6162fc20620a153
Vincent Michaud-Rioux <vincentm@nanoacademic.com>
2022-11-13 16:30:37 -0500

    Format test_grad.py and add plotting script.

--- tests/test_grad.py
+++ tests/test_grad.py
@@ -10,9 +10,13 @@ plt.style.use("bmh")
 engine = "lightning.kokkos"
 n_samples = 5
 
+
 def get_time(qnode, params):
-    globals_dict = {'grad': qml.grad, 'circuit': qnode, 'params': params}
-    return timeit.timeit("grad(circuit)(params)", globals=globals_dict, number=n_samples)
+    globals_dict = {"grad": qml.grad, "circuit": qnode, "params": params}
+    return timeit.timeit(
+        "grad(circuit)(params)", globals=globals_dict, number=n_samples
+    )
+
 
 def wires_scaling(n_wires, n_layers, num_threads=None):
 
@@ -31,15 +35,21 @@ def wires_scaling(n_wires, n_layers, num_threads=None):
 
     for i_wires in n_wires:
 
-        dev = qml.device(engine, wires=i_wires, kokkos_args=kokkos_args)
+        dev = qml.device(engine, wires=i_wires, kokkos_args=kokkos_args, batch_obs=True)
         dev_python = qml.device("default.qubit", wires=i_wires)
 
-        circuit_adjoint = qml.QNode(lambda x: circuit(x, wires=i_wires), dev, diff_method="adjoint")
-        circuit_ps = qml.QNode(lambda x: circuit(x, wires=i_wires), dev, diff_method="parameter-shift")
+        circuit_adjoint = qml.QNode(
+            lambda x: circuit(x, wires=i_wires), dev, diff_method="adjoint"
+        )
+        circuit_ps = qml.QNode(
+            lambda x: circuit(x, wires=i_wires), dev, diff_method="parameter-shift"
+        )
         # circuit_backprop = qml.QNode(lambda x: circuit(x, wires=i_wires), dev_python, diff_method="backprop")
 
         # set up the parameters
-        param_shape = qml.StronglyEntanglingLayers.shape(n_wires=i_wires, n_layers=n_layers)
+        param_shape = qml.StronglyEntanglingLayers.shape(
+            n_wires=i_wires, n_layers=n_layers
+        )
         params = rng.standard_normal(param_shape, requires_grad=True)
 
         t_adjoint.append(get_time(circuit_adjoint, params))
@@ -51,17 +61,24 @@ def wires_scaling(n_wires, n_layers, num_threads=None):
 
 if __name__ == "__main__":
 
-    num_threads = int(sys.argv[1])
+    num_threads = None
+    if len(sys.argv) > 0:
+        num_threads = int(sys.argv[1])
 
-    wires_list = [3,4,5]
+    wires_list = [15]
     n_layers = 3
-    adjoint_wires, ps_wires, backprop_wires = wires_scaling(wires_list, n_layers, num_threads)
-
+    adjoint_wires, ps_wires, backprop_wires = wires_scaling(
+        wires_list,
+        n_layers,
+        num_threads=num_threads,
+    )
+    with open("timings.dat", "a") as fid:
+        fid.write(f"{num_threads} {adjoint_wires[0]} {ps_wires[0]}\n")
     # Generating the graphic
     fig = plt.figure()
 
-    plt.plot(wires_list, adjoint_wires, '.-', label="adjoint")
-    plt.plot(wires_list, ps_wires, '.-', label="parameter-shift")
+    plt.plot(wires_list, adjoint_wires, ".-", label="adjoint")
+    plt.plot(wires_list, ps_wires, ".-", label="parameter-shift")
     # plt.plot(wires_list, backprop_wires, '.-', label="backprop")
 
     plt.legend()
